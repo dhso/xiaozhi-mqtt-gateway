08ed0ce187cb787f2ed89e7802c46fa17f967d1a
FAN-yeB <1442100690@qq.com>
2025-09-26 12:39:47 +0800

    Revert "update:更新远端mcp逻辑"
    This reverts commit d307685c969207b6de34ca3c7bcf9f0e464676d9.

--- app.js
+++ app.js
@@ -94,8 +94,10 @@ class WebSocketBridge extends Emitter {
                     const message = JSON.parse(data.toString());
                     if (message.type === 'hello') {
                         resolve(message);
-                    } else if (message.type === 'mcp') {
-                        this.connection.sendMqttMessage(JSON.stringify(message));
+                    } else if (message.type === 'mcp' &&
+                        this.connection.mcpCachedTools &&
+                        ['initialize', 'notifications/initialized', 'tools/list'].includes(message.payload.method)) {
+                        this.connection.onMcpMessageFromBridge(message);
                     } else {
                         this.connection.sendMqttMessage(JSON.stringify(message));
                     }
